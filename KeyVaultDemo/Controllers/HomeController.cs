97e3f15146708b0da3461d9a08efc8ddb9354170
toepoke <bitbucket@junge.f9.co.uk>
2018-04-14 18:31:00 +0100

    Initial version

---
+++ KeyVaultDemo/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Configuration;
+using System.Web.Http;
+using Microsoft.Azure.KeyVault;
+using Microsoft.Azure.KeyVault.Models;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using static Microsoft.Azure.KeyVault.KeyVaultClient;
+
+namespace KeyVaultDemo.Controllers {
+
+	// localhost:port/api/home
+	[RoutePrefix("api/")]
+	public class HomeController : ApiController {
+		// GET api
+		public async Task<IHttpActionResult> Get() {
+			AuthenticationCallback keyVaultCallback = new AuthenticationCallback(GetAccessTokenAsync);
+			KeyVaultClient keyVaultClient = new KeyVaultClient(keyVaultCallback);
+
+			string dbConnectionSecretId = WebConfigurationManager.AppSettings["DbConnectionSecretIdentifier"];
+			string adminPasswordSecretId = WebConfigurationManager.AppSettings["AdminPasswordSecretIdentifier"];
+
+			SecretBundle keyValueDbPassphraseSecret = await keyVaultClient.GetSecretAsync(dbConnectionSecretId);
+			SecretBundle keyValueAdminPasswordSecret = await keyVaultClient.GetSecretAsync(adminPasswordSecretId);
+
+			return Ok(new {
+				DbPassphrase = keyValueDbPassphraseSecret.Value,
+				AdminPassword = keyValueAdminPasswordSecret.Value
+			});
+		}
+
+
+		public static async Task<string> GetAccessTokenAsync(string authority, string resource, string scope) {
+			var clientId = WebConfigurationManager.AppSettings["ClientID"];
+			var clientSecret = WebConfigurationManager.AppSettings["ClientSecret"];
+
+			var context = new Microsoft.IdentityModel.Clients.ActiveDirectory.AuthenticationContext(authority);
+			ClientCredential credential = new ClientCredential(clientId, clientSecret);
+			AuthenticationResult result = await context.AcquireTokenAsync(resource, credential);
+
+			if (result == null)
+				throw new InvalidOperationException("Failed to obtain the JWT token");
+
+			return result.AccessToken;
+		}
+
+
+	} // HomeController
+
+
+
+} // Controllers
---
+++ KeyVaultDemo/Global.asax.cs
@@ -0,0 +1,10 @@
+﻿using System.Web.Http;
+
+namespace KeyVaultDemo {
+	public class WebApiApplication : System.Web.HttpApplication {
+		protected void Application_Start() {
+			GlobalConfiguration.Configure(WebApiConfig.Register);
+			//FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+		}
+	}
+}
